9c03dc4bc6b183d5ec56535435cec55d8d4633f9
Olanrewaju Eweyeju <ewewhatedo393@gmail.com>
2023-05-16 04:07:18 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# function that finds the biggest integer of a list.
+
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return
+
+    large = my_list[0]
+    for i in range(1, len(my_list)):
+        if large < my_list[i]:
+            large = my_list[i]
+        else:
+            continue
+    return large
