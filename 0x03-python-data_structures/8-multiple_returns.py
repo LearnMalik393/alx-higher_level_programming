b851f778ae6c104878b101d73eef4fa93397b44a
Olanrewaju Eweyeju <ewewhatedo393@gmail.com>
2023-05-16 04:02:11 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# function that returns a tuple with the length of a string and its first character.
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length == 0:
+        result = (0, None)
+        return result
+    else:
+        res = (length, sentence[0:1])
+        return res
